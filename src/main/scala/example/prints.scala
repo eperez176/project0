078290cd43be61bf39ccbb65edacd100723a5a21
emperez <emperez@bu.edu>
2021-10-11 15:33:12 -0500

    saving user object

--- src/main/scala/example/prints.scala
+++ src/main/scala/example/prints.scala
@@ -1,6 +1,16 @@
 package example;
 
-case object prints {
+// Imports
+import scala.io.Source;
+import java.io.File;
+import java.io._;
+import scala.io.StdIn.readLine;
+
+object prints extends App {
     def howdy: Unit = println("Howdy Yall");
     def no_howdy: Unit = println("No");
+
+    val filename = new File("Check.txt"); // This allows the creation of the file.
+    val user_writer = new FileWriter(filename, true); 
+    user_writer.close();
 }
