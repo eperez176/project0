05d0ac11d8f9ff916519802fcb03f5357a2aba93
emperez <emperez@bu.edu>
2021-10-12 15:53:35 -0500

    adding printBoard for minigame

--- src/main/scala/example/prints.scala
+++ src/main/scala/example/prints.scala
@@ -5,13 +5,15 @@ import scala.io.Source;
 import java.io.File;
 import java.io._;
 import scala.io.StdIn.readLine;
+import game.Game;
 // Tests certain functions seperately 
 object prints extends App {
     def howdy: Unit = println("Howdy Yall");
     def no_howdy: Unit = println("No");
-
+    Game.tic_tac_toe();
+    /* 
     val filename = new File("Check.txt"); // This allows the creation of the file.
     val user_writer = new FileWriter(filename, true); 
     user_writer.write("I eat burgers.")
-    user_writer.close();
+    user_writer.close(); */
 }
--- src/main/scala/game/Game.scala
+++ src/main/scala/game/Game.scala
@@ -3,11 +3,39 @@ package  game;
 case object Game { // Object having the game engines
     def tic_tac_toe() {
         println("Starting the tic tac toe game...");
+        var board = Array.ofDim[Char](3,3); // Creates the the simple board
+        for(i <- 0 to 2){
+            for(j <- 0 to 2)
+                board(i)(j) = 'X';
+        }
+        printBoard(board);
+
     }
     def black_jack(){
         println("Starting the black jack game...")
     }
     def memory_game(){
-        prinln("Starting the memory game...")
+        println("Starting the memory game...")
+    }
+
+    def printBoard(b: Array[Array[Char]]): Unit = {
+        print(b(0)(0));
+        print('|');
+        print(b(0)(1));
+        print('|');
+        println(b(0)(2));
+        println("-----")
+        print(b(1)(0));
+        print('|');
+        print(b(1)(1));
+        print('|');
+        println(b(1)(2));
+        println("-----")
+        print(b(2)(0));
+        print('|');
+        print(b(2)(1));
+        print('|');
+        println(b(2)(2));
+
     }
 }
