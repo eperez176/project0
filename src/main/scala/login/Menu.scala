78d7be36182d7fd25aaf6b2fecb3a76521e92967
emperez <emperez@bu.edu>
2021-10-13 11:12:51 -0500

    outline of main

--- src/main/scala/login/Menu.scala
+++ src/main/scala/login/Menu.scala
@@ -1,9 +1,19 @@
 package  login;
 
+import scala.io.StdIn.readLine;
+
 // Will create the menu and their functions
 case object Menu {
     def welcome: Unit = {
         println("Welcome!")
+    }
+    def options: Int = {
+        var out = 0;
+
+        println("What would you like to do:");
+        println("Search(1), game (2)");
+        out = scala.io.StdIn.readInt();
+        out;
     }
   
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,13 +2,19 @@ import example.prints;
 import login.Login;
 import login.Menu;
 
+// Admin user that has extra perks like looking at the current users, delete, insert etc.
+// While having users only be able to use
+// To show delete and update capabilities (if not wanting to work with the car.csv)
 object main extends App {
     var isRealUser = false;
+    var opt = 0;
 
     println("Please login")
 
     // Sign in/ Sign up before continuing
     isRealUser = Login.login();
-    if(isRealUser)
+    if(isRealUser) {
         Menu.welcome;
+        opt = Menu.options;
+    }
 }
