eec3c4460f5e15cca52612655b6f36da65d7899e
emperez <emperez@bu.edu>
2021-10-17 15:28:37 -0500

    Adding conditions to the search query

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -11,6 +11,7 @@ object Hello extends App {
         var model_name = "";
         var color = "";
         var m_price = 0.0;
+        var year_p = 0;
 
         println("Loading the search queue...");
         val client: MongoClient = MongoClient();
@@ -20,10 +21,10 @@ object Hello extends App {
         var opt = 0;
         println("\nSearch options include: Find (1), average (2)\n");
         opt = scala.io.StdIn.readInt();
-        
+        var out = collection.find().results; // Pre-load to have variable set
         // Search functions
         if(opt == 1) {
-            println("For unwanted options: Enter 'N/A' \n");
+            println("For unwanted options: Enter '0' \n");
 
             println("Car Manufacturer");
             manuf_name = scala.io.StdIn.readLine();
@@ -31,15 +32,62 @@ object Hello extends App {
             model_name = scala.io.StdIn.readLine();
             println("Max Price")
             m_price = scala.io.StdIn.readDouble();
-            val out = collection.find(and(equal("manufacturer_name", manuf_name), equal("model_name", model_name), lt("price_usd", m_price))).results;
-            for(single_doc <- out) {
-              var mnn = single_doc.get("model_name").get.asString().getValue();
-              var mn = single_doc.get("manufacturer_name").get.asString().getValue();
-              var price = single_doc.get("price_usd").get.asDouble().getValue();
-              var od_val = single_doc.get("odometer_value").get.asInt32().getValue();
-              var year = single_doc.get("year_produced").get.asInt32().getValue();
-              println(s"$mnn, $mn: Price: $price with odometer value: $od_val and year produce: $year");
-            }
 
+            /*val out = collection.find(and(equal("manufacturer_name", manuf_name), equal("model_name", model_name), lt("price_usd", m_price))).results;
+            for(single_doc <- out) {
+              var manuf_n = single_doc.get("manufacturer_name").get.asString().getValue();
+              var model_n = single_doc.get("model_name").get.asString().getValue();
+              var price = single_doc.get("price_usd").get.asDouble().getValue();
+              var year = single_doc.get("year_produced").get.asInt32().getValue();
+            }
+            */
+
+              if(manuf_name != "0") {
+                if (model_name != "0") {
+                  if(m_price != 0) {
+                    out = collection.find(and(equal("manufacturer_name", manuf_name), equal("model_name", model_name), lte("price_usd",m_price))).results;
+                  }
+                  else {
+                    out = collection.find(and(equal("manufacturer_name", manuf_name), equal("model_name", model_name))).results;
+                  }
+                }
+                else {
+                  if(m_price != 0) {
+                    out = collection.find(and(equal("manufacturer_name", manuf_name), lte("price_usd",m_price))).results;
+                  }
+                  else {
+                    out = collection.find(equal("manufacturer_name", manuf_name)).results;
+                  }
+                }
+              }
+              else {
+                if(model_name != "0") {
+                  if(m_price != 0) {
+                    out = collection.find(and(equal("model_name", model_name), lte("price_usd",m_price))).results;
+                  }
+                  else {
+                    out = collection.find(equal("model_name", model_name)).results;
+                  }
+
+                }
+                else {
+                  if(m_price != 0) {
+                    out = collection.find(equal("price_usd", m_price)).results;
+                  }
+                  else { // All resuts
+                    out = collection.find().results;
+                  }
+                }
+              }
+
+            // Prints the outcome of the search query  
+            for(single_doc <- out) {
+              var manuf_n = single_doc.get("manufacturer_name").get.asString().getValue();
+              var model_n = single_doc.get("model_name").get.asString().getValue();
+              var price = single_doc.get("price_usd").get.asDouble().getValue();
+              var year = single_doc.get("year_produced").get.asInt32().getValue();
+              println(s"$model_n, $manuf_n, $price, $year");
+            }
         }
+        client.close();
 }
